eb4232d60ec30243cfb89091717badeb7b33f89a
Lena <summyalena@gmail.com>
2023-01-29 18:33:46 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Headers from './components/headers';
+import Home from './components/home'
+
+function App() {
+  return (
+    <div className="App">
+     <Headers/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
