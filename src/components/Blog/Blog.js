00b97f783431292a82ed3770447610dac5032923
Lena <summyalena@gmail.com>
2023-02-08 15:20:19 +0100

    styling blog page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import styled from 'styled-components'
+import styles from './Blog.module.css'
+
+const Header = styled.div`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+  font-size: 2rem;
+  font-family: 'Pacifico';
+  color: var(--main-color);
+`
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.container}>
+     <div className={styles.wrapper}>
+     <Header>
+           My Blog..
+        </Header>
+        <div className={styles.Box}>
+          <div className={styles.miniBox}>
+             <div className={styles.image}>
+              
+             </div>
+              <div className={styles.text}>
+                This Blog is an introductory article for anyone new to Vercel and Netlify. This article guides a newbie to successfully deploy a work with no stress
+              </div>
+              <button>
+                Click Me
+              </button>
+          </div>
+
+          <div className={styles.miniBox}>
+             <div className={styles.image}>
+              
+             </div>
+              <div className={styles.text}>
+                This Blog is an introductory article for anyone new to Vercel and Netlify. This article guides a newbie to successfully deploy a work with no stress
+              </div>
+              <button>
+                Click Me
+              </button>
+          </div>
+          
+          </div> 
+     </div>
+      </div>
   )
 }
 
--- src/components/subcomponents/BlogLink.js
+++ src/components/subcomponents/BlogLink.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
 
 const Blog = styled.div`
     position: absolute;
     top: 50%;
-    right: -50%;
-    display: flex;
+    right: 0%;
+    transform: rotate(90deg) translate(-50%, -50%);
+`
+
+const NavBlog = styled(NavLink)`
+   text-decoration: none;
+   color: var(--main-color);
 `
 const BlogLink = () => {
   return (
     <Blog>
+      <NavBlog to='/Main'>
       Blog
+      </NavBlog>
     </Blog>
   )
 }
