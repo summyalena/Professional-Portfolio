cb62ea4233f4d47cbf84136420f99bf0f2578f78
Lena <summyalena@gmail.com>
2023-02-08 15:19:00 +0100

    styling about page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
-import AboutPage from './components/About/AboutPage';
-import Contact from './components/contact/contact';
 import Headers from './components/headers/headers';
 import Headline from './components/headline/headline';
-import Projects from './components/Projects/Projects';
-import Footer from './components/Footer/Footer';
-import { Route, Routes } from 'react-router';
+
 
 function App() {
   return (
     <div className="App">
      <Headers/>
-      <Routes>
-   <Route  exact path='/' element={<Headline/>}/>  
-    <Route exact path='/About' element={ <AboutPage/>}/>
-    <Route exact path='/Projects' element={<Projects/>}/>
-    <Route exact path='/Contact' element={ <Contact/>} />
-    <Route exact path='/Footer' element={<Footer/>}/>
-      </Routes>
-    
+     <Headline/>
     </div>
   );
 }
--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import styles from './about.module.css'
+import styled from 'styled-components'
 
+const Header = styled.div`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+  font-size: 2rem;
+  font-family: 'Pacifico';
+  color: var(--main-color);
+`
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Header>
+           About Me..
+        </Header>
+        <div className={styles.overall}>
+        <div className={styles.writeup}>
+          <div className={styles.write}>
+      I am a software Developer who resides in Nigeria. I am a fullstack developer who recently graduated from Microverse which is a bootcamp Programme. I also graduated from a degree programme in Computer Science in a prestigious University in NIgeria.
+      </div>
+      </div>
+      <div className={styles.images}>
+        <div className={styles.imageBox}>
+            <div className={styles.imageBoxx}>
+
+            </div>
+        </div>
+        </div>
+      </div>
+      </div>
+      </div>
   )
 }
 
