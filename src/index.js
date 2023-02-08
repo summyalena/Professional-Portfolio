81b89181302d40aacd4c71ac4bece14cf2ae0ff6
Lena <summyalena@gmail.com>
2023-02-08 15:18:15 +0100

    adding assests

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import AboutPage from './components/About/AboutPage';
+import Contact from './components/contact/contact';
+import Headline from './components/headline/headline';
+import Projects from './components/Projects/Projects';
+import Footer from './components/Footer/Footer';
+import Blog from './components/Blog/Blog';
+import Main from './components/main/main';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    <Routes>
+      <Route exact path='/' element={ <App />}/>
+   <Route  exact path='/Home' element={<Headline/>}/>  
+   <Route exact path='/Main' element={<Main/>}/>
+    <Route exact path='/About' element={ <AboutPage/>}/>
+    <Route exact path='/Projects' element={<Projects/>}/>
+    <Route exact path='/Blog' element={<Blog/>}/>
+    <Route exact path='/Contact' element={ <Contact/>} />
+    <Route exact path='/Footer' element={<Footer/>}/>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
