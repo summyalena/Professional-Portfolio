925ed2e2d06bf60df0af67504476d0bc70177f1d
Lena <summyalena@gmail.com>
2023-02-08 15:22:32 +0100

    adding about components and writeup

---
+++ src/components/subcomponents/AboutLink.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
+
+const AboutLink = () => {
+    const About = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 15%;
+    text-decoration: none;
+    transform: rotate(-90deg) translate(-50%, -50%);
+`
+    const NavAbout = styled(NavLink)`
+       text-decoration: none;
+       color: #fee715;
+    `
+  return (
+    <div>
+        <About>
+        <NavAbout to='/About'>
+         About
+         </NavAbout>
+        </About>
+    </div>
+  )
+}
+
+export default AboutLink
--- src/components/subcomponents/writeup.js
+++ src/components/subcomponents/writeup.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import TypewriterComponent from 'typewriter-effect'
 
 const Writes = styled.div`
-
+   display: flex;
+   justify-content: center;
+   width: 300px;
+   align-items: center;
+   padding: 0 0 0 35rem;
+   font-family: Playfair Display;
+   font-size: 2rem;
 `
 const Writeup = () => {
   return (
     <Writes>
-        <h4>Hello, I am Assumpta Okolike and I am a software developer. I can bring to life any software and website. I reside in Nigeria</h4>
+            <TypewriterComponent 
+                options={{
+                    strings: ["Hello!", "Welcome to my portfolio page!", "Have a nice day! "],
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 50,
+                }}
+        />   
     </Writes>
   )
 }
