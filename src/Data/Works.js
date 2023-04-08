080c11de4c7d2cbab202078c337ce3a2dcc27d73
Lena <summyalena@gmail.com>
2023-04-08 10:52:31 +0100

    Add work and fade in section

---
+++ src/Data/Works.js
@@ -0,0 +1,53 @@
+const Work = [
+    {
+    id: 1,
+    name: 'Twitter Clone' ,
+    description: 'This is a project that was built using Nextjs',
+    image: 'http:www.google.com',
+    link: 'http://www.google.com',
+    techStack: 'Next.js | Tailwind CSS | Firebase ',
+    githubLink: 'https://github.com/summyalena/'
+},
+ 
+{
+    id: 2,
+    name: 'Conference',
+    description: 'This is a website created to share awareness, created with html, css and javascript',
+    image: 'http://www.google.com',
+    link: 'https://www.google.com',
+    techStack: 'Javascript | CSS | HTML ',
+    githubLink: 'https://github.com/summyalena/'
+},
+
+{
+    id: 2,
+    name: 'Conference',
+    description: 'This is a website created to share awareness, created with html, css and javascript',
+    image: 'http://www.google.com',
+    link: 'https://www.google.com',
+    techStack: 'Javascript | CSS | HTML ',
+    githubLink: 'https://github.com/summyalena/'
+},
+
+{
+    id: 2,
+    name: 'Conference',
+    description: 'This is a website created to share awareness, created with html, css and javascript',
+    image: 'http://www.google.com',
+    link: 'https://www.google.com',
+    techStack: 'Javascript | CSS | HTML ',
+    githubLink: 'https://github.com/summyalena/'
+},
+
+{
+    id: 2,
+    name: 'Conference',
+    description: 'This is a website created to share awareness, created with html, css and javascript',
+    image: 'http://www.google.com',
+    link: 'https://www.google.com',
+    techStack: 'Javascript | CSS | HTML ',
+    githubLink: 'https://github.com/summyalena/'
+}
+]
+
+export default Work;
