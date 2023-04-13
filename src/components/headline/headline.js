6462a3aca3b3cc5e544273a7e8d504a005b69d32
Lena <summyalena@gmail.com>
2023-04-13 23:03:40 +0100

    modification

--- src/components/headline/headline.js
+++ src/components/headline/headline.js
@@ -1,21 +1,21 @@
 import React, {useState} from 'react'
 import styles from './Headline.module.css';
 import SocialLinks from '../subcomponents/socialLinks';
-import Writeup from '../subcomponents/writeup';
+// import Writeup from '../subcomponents/writeup';
 
 function Headline() {
-  const [click, setClick] = useState(false);
+  // const [click, setClick] = useState(false);
 
-  const handleClick = () => {
-      setClick(!click);
-  }
+  // const handleClick = () => {
+  //     setClick(!click);
+  // }
   
   return (
     <>
       <div className={styles.container}>
         <SocialLinks/>
         
-        <Writeup click={click} clickOn={handleClick}/>
+        {/* <Writeup click={click} clickOn={handleClick}/> */}
       
     </div>
     </>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
