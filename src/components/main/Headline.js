a6b457c44bb8f74928a31fd22e5cfdc016fbecb8
Lena <summyalena@gmail.com>
2023-04-14 19:27:08 +0100

    mild changes

--- src/components/main/Headline.js
+++ src/components/main/Headline.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './main.module.css';
 import Contact from '../contact/contact';
 import AboutPage from '../About/AboutPage';
-import Headers from '../headers/Headers';
+
 import MainHeadline from './mainHeadline';
 import Projects from '../Projects/Projects';
 import Blog from '../Blog/Blog';
@@ -17,7 +17,7 @@ const Main = () => {
   return (
     <>
     <motion.div className="progress-bar" style={{scaleX: scrollYProgress}}/>
-    <Headers/>
+    {/* <Headers/> */}
     <div className={styles.container}>
         <div className={styles.wrapper}>
     <MainHeadline/>
