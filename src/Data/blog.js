6dcf8a681f3fd1064a58f31aacc1ab7ff797a3cc
Lena <summyalena@gmail.com>
2023-06-26 18:02:14 +0100

    Add content

--- src/Data/blog.js
+++ src/Data/blog.js
@@ -9,28 +9,6 @@ const Blogs = [
         link: 'https://www.google.com'
     },
 
-    {
-        id: 2,
-        name: 'Deploying my first project on Vercel',
-        description: 'This talks about to deploy your Nextjs first project on Vercel',
-        image: medium,
-        link: 'https://www.google.com'
-    },
-
-    {
-        id: 3,
-        name: 'Deploying my first project on Vercel',
-        description: 'This talks about to deploy your Nextjs first project on Vercel',
-        image: medium,
-        link: 'https://www.google.com'
-    },
-    {
-        id: 4,
-        name: 'Deploying my first project on Vercel',
-        description: 'This talks about to deploy your Nextjs first project on Vercel',
-        image: medium,
-        link: 'https://www.google.com'
-    }
 ]
 
 export default Blogs;
